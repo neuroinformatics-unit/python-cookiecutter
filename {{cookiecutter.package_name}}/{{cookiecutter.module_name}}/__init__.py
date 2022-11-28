15e63fc6e03ae5b6de6159a4ec371a094fac423e
Adam Tyson <code@adamltyson.com>
2022-11-28 17:52:39 +0000

    Update __init__.py

--- {{cookiecutter.package_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("python-packaging-at")
+    __version__ = version("{{cookiecutter.package_name}}")
 except PackageNotFoundError:
     # package is not installed
     pass
