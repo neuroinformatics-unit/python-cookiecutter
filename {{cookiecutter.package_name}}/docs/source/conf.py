7b8188fe99009e99ede52974e1d3844ce0355622
niksirbi <niko.sirbiladze@gmail.com>
2022-12-09 15:10:52 +0000

    updated shinx config

--- {{cookiecutter.package_name}}/docs/source/conf.py
+++ {{cookiecutter.package_name}}/docs/source/conf.py
@@ -16,15 +16,78 @@ release = version('{{cookiecutter.package_name}}')
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'myst_parser',
+    'numpydoc',
+    'nbsphinx',
+]
 
+# Configure the myst parser to enable cool markdown features
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+# Automatically add achors to markdown headings
+myst_heading_anchors = 2
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
-
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "**.ipynb_checkpoints",
+    # to ensure that include files (partial pages) aren't built, exclude them
+    # https://github.com/sphinx-doc/sphinx/issues/1965#issuecomment-124732907
+    "**/includes/**",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'pydata_sphinx_theme'
+html_title = '{{cookiecutter.package_name}}'
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+## Cutomize the theme
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "{{ cookiecutter.github_repository_url }}",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
+
+# Redirect the webpage to another URL
+# Sphinx will create the appropriate CNAME file in the build directory
+# The default is the URL of the GitHub pages
+# https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
+html_baseurl = 'http(s)://{{cookiecutter.github_username_or_organization}}.github.io/{{cookiecutter.package_name}}'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
