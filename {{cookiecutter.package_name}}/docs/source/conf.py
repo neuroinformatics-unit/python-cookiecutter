ddc4fb9e93d48410437ceb12be9fa88d2943a81e
niksirbi <niko.sirbiladze@gmail.com>
2022-12-20 11:18:15 +0000

    fix some linting issues

--- {{cookiecutter.package_name}}/docs/source/conf.py
+++ {{cookiecutter.package_name}}/docs/source/conf.py
@@ -8,24 +8,24 @@
 
 from importlib.metadata import version
 
-project = '{{cookiecutter.package_name}}'
-copyright = '2022, {{cookiecutter.full_name}}'
-author = '{{cookiecutter.full_name}}'
-release = version('{{cookiecutter.package_name}}')
+project = "{{cookiecutter.package_name}}"
+copyright = "2022, {{cookiecutter.full_name}}"
+author = "{{cookiecutter.full_name}}"
+release = version("{{cookiecutter.package_name}}")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'myst_parser',
-    'numpydoc',
-    'nbsphinx',
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "numpydoc",
+    "nbsphinx",
 ]
 
 # Configure the myst parser to enable cool markdown features
@@ -48,7 +48,7 @@ myst_enable_extensions = [
 myst_heading_anchors = 2
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -62,10 +62,10 @@ exclude_patterns = [
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'pydata_sphinx_theme'
-html_title = '{{cookiecutter.package_name}}'
+html_theme = "pydata_sphinx_theme"
+html_title = "{{cookiecutter.package_name}}"
 
-## Cutomize the theme
+# Cutomize the theme
 html_theme_options = {
     "icon_links": [
         {
@@ -73,19 +73,21 @@ html_theme_options = {
             "name": "GitHub",
             # URL where the link will redirect
             "url": "{{ cookiecutter.github_repository_url }}",  # required
-            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            # Icon class (if "type": "fontawesome"),
+            # or path to local image (if "type": "local")
             "icon": "fa-brands fa-github",
             # The type of image to be used (see below for details)
             "type": "fontawesome",
         }
-   ]
+    ]
 }
 
 # Redirect the webpage to another URL
 # Sphinx will create the appropriate CNAME file in the build directory
 # The default is the URL of the GitHub pages
 # https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
-html_baseurl = 'http(s)://{{cookiecutter.github_username_or_organization}}.github.io/{{cookiecutter.package_name}}'
+github_user = "{{cookiecutter.github_username_or_organization}}"
+html_baseurl = f"http(s)://{github_user}.github.io/{project}"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
