bd70c49d9ab00417fa36a09cb0e36ba518076737
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-01-27 15:21:26 +0000

    remove all non-doc files and folders if doc is not set

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,14 @@
 import shutil
-create_docs = '{{ cookiecutter.create_docs }}'
+import os
+
+create_docs = "{{ cookiecutter.create_docs }}"
+module_name = "{{ cookiecutter.module_name }}"
 
 if __name__ == "__main__":
     if create_docs != "yes":
         shutil.rmtree("docs", ignore_errors=True)
+        os.remove(".github/workflows/check_docs.yml")
+        os.remove(".github/workflows/publish_docs.yml")
+        os.remove(f"{module_name}/greetings.py")
+        os.remove(f"{module_name}/math.py")
     shutil.rmtree("licenses", ignore_errors=True)
