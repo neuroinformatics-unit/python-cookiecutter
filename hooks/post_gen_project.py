42d38be6e7018a0a01b3beba54838a71768f66f1
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-01-27 15:17:30 +0000

    fix syntax on cleanup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,7 @@
 import shutil
+create_docs = '{{ cookiecutter.create_docs }}'
 
 if __name__ == "__main__":
-    { % if cookiecutter.create_docs == "yes" - %}
-    shutil.rmtree("docs", ignore_errors=True)
-    { % - endif %}
+    if create_docs != "yes":
+        shutil.rmtree("docs", ignore_errors=True)
     shutil.rmtree("licenses", ignore_errors=True)
