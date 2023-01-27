cf4ba222cbd8c951e2597ca89b551b8af1fabd0c
JoeZiminski <joseph.j.ziminski@gmail.com>
2023-01-27 15:15:51 +0000

    try optional cleanup in hook with cookiecutter conditional

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,7 @@
 import shutil
 
 if __name__ == "__main__":
+    { % if cookiecutter.create_docs == "yes" - %}
+    shutil.rmtree("docs", ignore_errors=True)
+    { % - endif %}
     shutil.rmtree("licenses", ignore_errors=True)
