7e9b918c2360a11111f5d2c9adfea78f12a881c2
JoeZiminski <Joseph.j.ziminski@gmail.com>
2023-02-13 16:08:50 +0000

    fix sytax error on linux just introduced

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -59,7 +59,7 @@ def pip_install(package_path):
     )
 
     # Install package
-    dev_formatting = ".[dev]" if platform.system() == "Windows" else "'[.dev]'"
+    dev_formatting = ".[dev]" if platform.system() == "Windows" else "'.[dev]'"
     cmd = f"pip install -e {dev_formatting}"
     result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     stdout, __ = result.communicate()
