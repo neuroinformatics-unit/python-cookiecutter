00ad06be75ffc758846d7d9c17fedfc462332f07
JoeZiminski <Joseph.j.ziminski@gmail.com>
2023-03-02 17:00:54 +0000

    convert create_docs conditionals to bool

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,7 @@ create_docs = "{{ cookiecutter.create_docs }}"
 module_name = "{{ cookiecutter.module_name }}"
 
 if __name__ == "__main__":
-    if create_docs != "yes":
+    if not create_docs:
         shutil.rmtree("docs", ignore_errors=True)
         os.remove(".github/workflows/check_docs.yml")
         os.remove(".github/workflows/publish_docs.yml")
